d219c60f85d5187f3e61a01e12335736cf2c497a
Md. Sumon Islam <developer.sumon@gmail.com>
2018-09-08 02:27:11 +0600

    How much is your $100 worth after 7 years?

--- script.py
+++ script.py
@@ -3,3 +3,16 @@ print(5 / 8)
 
 # Put code below here
 print(7 + 10)
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100 * 1.1 ** 7)
