cae67f9167bd6158b3184b949360a5dea1aca80e
Md. Sumon Islam <developer.sumon@gmail.com>
2018-09-08 02:46:24 +0600

    Calulations with variables

--- script.py
+++ script.py
@@ -31,3 +31,16 @@ name    = "John"       # A string
 print counter
 print miles
 print name
+
+# Calulations with variables
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.10
+
+# Calculate result
+result = savings * factor ** 7
+
+# Print out result
+print(result)
