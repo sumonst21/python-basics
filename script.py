7c301d2660efc4ea8410064f2b896e94ea1761ed
Md. Sumon Islam <developer.sumon@gmail.com>
2018-09-08 03:03:31 +0600

    Operations with other types

--- script.py
+++ script.py
@@ -51,3 +51,20 @@ desc = "compound interest" # A str or string
 
 # Create a variable profitable
 profitable = True # a bool or boolean
+
+# Operations with the other types
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of factor and savings to year1
+year1 = savings * factor
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
